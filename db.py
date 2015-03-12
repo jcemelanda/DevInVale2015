39db89ba673d3686cd5c0208e21e0e582cf369e8
iuryAlves <satriani-16@hotmail.com>
2015-03-12 11:30:32 -0300

    pep8

--- db.py
+++ db.py
@@ -5,40 +5,41 @@ import sqlite3
 
 class DB(object):
 
-	def __init__(self, database):
-		self.connection  = None
-		self.database = database
-		self._create_score_table()
+    def __init__(self, database):
+        self.connection = None
+        self.database = database
+        self._create_score_table()
 
-	def _connect(self):
-		self.connection = sqlite3.connect(self.database)
+    def _connect(self):
+        self.connection = sqlite3.connect(self.database)
 
-	def _create_score_table(self):
-		self._connect()
+    def _create_score_table(self):
+        self._connect()
 
-		with self.connection as con:
-			try:
-				con.execute('CREATE TABLE scores (score INTEGER, player TEXT)')
-			except sqlite3.OperationalError:
-				pass
-		self.connection.close()
+        with self.connection as con:
+            try:
+                con.execute('CREATE TABLE scores (score INTEGER, player TEXT)')
+            except sqlite3.OperationalError:
+                pass
+        self.connection.close()
 
-	def save_score(self, score, player):
-		values = (score, player, )
+    def save_score(self, score, player):
+        values = (score, player, )
 
-		self._connect()
-		with self.connection as con:
-			con.execute("INSERT INTO scores VALUES (?, ?)", values)
+        self._connect()
+        with self.connection as con:
+            con.execute("INSERT INTO scores VALUES (?, ?)", values)
 
-		self.connection.close()
+        self.connection.close()
 
-	def get_scores(self, player):
-		values = (player, )
-		self._connect()
-		scores = None
-		with self.connection as con:
-			scores = con.execute("SELECT * FROM scores where player=?", values).fetchmany(size=10)
+    def get_scores(self, player):
+        values = (player, )
+        self._connect()
+        scores = None
+        with self.connection as con:
+            scores = con.execute(
+                "SELECT * FROM scores where player=?", values).fetchmany(size=10)
 
-		self.connection.close()
+        self.connection.close()
 
-		return scores
+        return scores
